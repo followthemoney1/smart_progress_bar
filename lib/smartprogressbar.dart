86052ea2d3845eea81f8d665e791abb4cff4fe53
D. Diachenko <emigrantdd@gmail.com>
2020-06-22 19:04:18 +0300

    about project

---
+++ lib/smartprogressbar.dart
@@ -0,0 +1,56 @@
+library smartprogressbar;
+
+import 'package:flutter/cupertino.dart';
+import 'package:native_progress_hud/native_progress_hud.dart';
+
+class SmartProgressBar {
+  static void showWhileRunning({@required Function onRun}) {}
+}
+
+extension SmartProgressBarFunctionExt on Function {
+  Function withSmartProgressBar(
+      {String text,
+      String backgroundColor = "#000000",
+      String textColor = "#ffffff"}) {
+    var res;
+//    Future.delayed(Duration(seconds: 8), () => NativeProgressHud.hideWaiting());
+    try {
+      text == null
+          ? NativeProgressHud.showWaiting(
+              backgroundColor: backgroundColor, textColor: textColor)
+          : NativeProgressHud.showWaitingWithText(text,
+              backgroundColor: backgroundColor, textColor: textColor);
+      res = this;
+    } catch (e) {
+      print(e);
+    } finally {
+      Future.delayed(Duration(seconds: 1),
+          () => NativeProgressHud.hideWaiting()); //for visibility
+    }
+    return res;
+  }
+}
+
+extension SmartProgressBarFutureExt<T> on Future<T> {
+  Future<T> withSmartProgressBar(
+      {String text,
+      String backgroundColor = "#000000",
+      String textColor = "#ffffff"}) async {
+    var res;
+//    Future.delayed(Duration(seconds: 8), () => NativeProgressHud.hideWaiting());
+    try {
+      text == null
+          ? await NativeProgressHud.showWaiting(
+              backgroundColor: backgroundColor, textColor: textColor)
+          : await NativeProgressHud.showWaitingWithText(text,
+              backgroundColor: backgroundColor, textColor: textColor);
+      res = await this;
+    } catch (e) {
+      print(e);
+    } finally {
+      await Future.delayed(Duration(seconds: 1),
+          () => NativeProgressHud.hideWaiting()); //for visibility
+    }
+    return res;
+  }
+}
